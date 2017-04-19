1eaf6fe5fa1bea7631f0460083b740812268f649
phoityne <phoityne.hs.gmail.com>
2017-04-19 23:45:18 +0900

    initial commit.

---
+++ vscode-extension/phoityne-vscode.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export PHOITYNE_PATH=`which phoityne-vscode`
+
+if [ "X" = "X$PHOITYNE_PATH" ]; then
+  echo -e "Content-Length: 194\r\n\r"
+  echo '{"command":"initialize","success":false,"request_seq":1,"seq":1,"type":"response","message":"phoityne-vscode is not found. Run `stack install phoityne-vscode`, and put it to PATH environment."}'
+
+  exit 1
+fi
+
+phoityne-vscode
+
+
